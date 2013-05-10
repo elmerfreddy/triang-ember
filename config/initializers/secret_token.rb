0375ffe4f526e499aeb1b1a140535bf958d9f034
Elmer Mendoza <defreddyelmer@gmail.com>
2013-05-10 14:39:55 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TriangEmber::Application.config.secret_key_base = 'a670e682c8d96cbd2592e443e0f1b604e220282b87daa790c8d4d23f720070c4867b4781e3af24f95b3f16c75e69264e7b1650a4a0109cd6b7ed3ae9169fd529'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TriangEmber::Application.config.session_store :cookie_store, key: '_triang-ember_session'
